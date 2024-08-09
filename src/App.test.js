382c8b3140d7ede97aadcad55544c550851f4be8
karin0098 <karin0098shoemaker@outlook.com>
2024-08-09 16:37:49 +0530

    Create App.test.js

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders the app', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Dollar Tree Compass/i);
+  expect(linkElement).toBeInTheDocument();
+});
