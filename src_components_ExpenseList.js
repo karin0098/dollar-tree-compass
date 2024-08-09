7ca46ede0396181a64e2b08683ae1eb48de93ec7
karin0098 <karin0098shoemaker@outlook.com>
2024-08-09 16:39:40 +0530

    Create src_components_ExpenseList.js

---
+++ src_components_ExpenseList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ExpenseList.css';
+
+function ExpenseList() {
+  // Placeholder for the list of expenses
+  return (
+    <div className="ExpenseList">
+      <h2>Expense List</h2>
+      {/* Expense items will be rendered here */}
+    </div>
+  );
+}
+
+export default ExpenseList;
